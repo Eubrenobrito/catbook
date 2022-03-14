e904f537b17c07a80416411b24d3015322c0bb6b
BRENO@Breno <brenobrito_13@hotmail.com>
2022-03-14 20:37:58 -0300

    bug fix github no webstorm

--- frontend/src/app/home--routing/home--routing.module.ts
+++ frontend/src/app/home--routing/home--routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {Routes, RouterModule} from "@angular/router";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "../home/home.component";
+import {LoginComponent} from "../home/login/login.component";
 
 const routes: Routes = [{
   path: '',
   component: HomeComponent,
+  children: [
+    {
+      path:'',
+      component: LoginComponent
+    }
+  ]
 }];
 
 @NgModule({
