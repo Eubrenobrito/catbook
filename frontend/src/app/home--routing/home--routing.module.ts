46655c4bab8263c7479f81d2a05688d52efa7e46
Breno Brito <brenobrito_13@hotmail.com>
2022-04-21 18:56:54 -0300

    adicionando tela de cadastro

--- frontend/src/app/home--routing/home--routing.module.ts
+++ frontend/src/app/home--routing/home--routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "../home/home.component";
 import {LoginComponent} from "../home/login/login.component";
+import {NovoUsuarioComponent} from "../home/novo-usuario/novo-usuario.component";
 
 const routes: Routes = [{
   path: '',
@@ -10,6 +11,11 @@ const routes: Routes = [{
     {
       path:'',
       component: LoginComponent
+    },
+    {
+      path: 'novousuario',
+      component: NovoUsuarioComponent,
+
     }
   ]
 }];
